fadcd9653b91a439ac6a367b5fc5ec2844e2f287
Mr-JZ <jan.zisenis@gmail.com>
2023-02-02 21:46:25 +0100

    added a javadoc comment creator for methods and changed some keymapping

--- nvim/lua/mrjz/remap.lua
+++ nvim/lua/mrjz/remap.lua
@@ -11,6 +11,9 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+vim.keymap.set({"n", "i"}, "<leader>b", vim.cmd.NvimTreeToggle)
+
+vim.keymap.set("n", "<leader>cm", ":luafile ~/.config/nvim/lua/mrjz/javadocMethod.lua<CR>")
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
